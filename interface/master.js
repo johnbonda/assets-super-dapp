77cc0bc6614a871c1b91acce170281db3b13f65b
chandini <chandini@belfricsbt.com>
2018-12-04 13:09:49 +0530

    uncommented

--- interface/master.js
+++ interface/master.js
@@ -15,24 +15,24 @@ app.route.post('/user/exists',async function(req,cb){
     }
     return "0";
 });
-// app.route.post('/user/login', async function (req, cb) {
-//     var params = {
-//         email: req.query.email,
-//         password: req.query.password,
-//         totp:totp
-//     };
-//     var response = await SwaggerCall.call('POST', `/api/v1/login`, params);//staging api
-//     return response;
-// });
+app.route.post('/user/login', async function (req, cb) {
+    var params = {
+        email: req.query.email,
+        password: req.query.password,
+        totp:totp
+    };
+    var response = await SwaggerCall.call('POST', `/api/v1/login`, params);//staging api
+    return response;
+});
 
-// app.route.post('/user/kyclogin',async function(req,cb){
-// var params={
-//     secret:req.query.secret  
-// };
-// var token=req.query.token;
-// var response= await SwaggerCall.call('POST',`/api/v1/hyperledger/login)`,token,params,);
-// return response;
-// });
+app.route.post('/user/kyclogin',async function(req,cb){
+var params={
+    secret:req.query.secret  
+};
+var token=req.query.token;
+var response= await SwaggerCall.call('POST',`/api/v1/hyperledger/login)`,token,params,);
+return response;
+});
 
 app.route.post('/user/dappid',async function(req,cb){
         var result = await app.model.Mapping.findOne({
