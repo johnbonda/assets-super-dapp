fc36c25e46bfd6d34ec6797aa2333ef6cadf4a50
chandini <chandini@belfricsbt.com>
2018-12-04 16:13:46 +0530

    added ton

--- interface/master.js
+++ interface/master.js
@@ -2,9 +2,11 @@ var SwaggerCall = require('../utils/SwaggerCall.js');
 var hlCall=require('../utils/hlCall.js');
 var headerCall=require('../utils/headerCall.js');
 app.route.post('/user/exists',async function(req,cb){
-   var params={
+  if(req.query.email===undefined) return "invalid input";
+    var params={
         email:req.query.email
    }
+
    var result=await app.model.Mapping.exists(params);
    if(!result){
         var response = await SwaggerCall.call('GET', '/api/v1/user/exist?email=' + params.email, params);  //staging api
@@ -30,7 +32,7 @@ app.route.post('/user/login', async function (req, cb) {
         });
         console.log("added");
     }
-    var response = await SwaggerCall.call('POST', `/api/v1/login`, params);//staging api
+    var response = await SwaggerCall.call('POST', '/api/v1/login', params);//staging api
     return response;
 });
 
@@ -39,7 +41,7 @@ var params={
     secret:req.query.secret  
 };
 var token=req.query.token;
-var response= await hlCall.call('POST',`/api/v1/hyperledger/login`,params,token);
+var response= await hlCall.call('POST','/api/v1/hyperledger/login',params,token);
 return response;
 });
 
@@ -57,7 +59,7 @@ app.route.post('/user/dappid',async function(req,cb){
 });
 app.route.post('/user/wallet',async function(req,cb){
     var token=req.query.token
-    var result=await headerCall.call('GET',`/api/v1/wallets`,token);
+    var result=await headerCall.call('GET','/api/v1/wallets',token);
     return result;
 });
 app.route.post('/user/balance',async function(req,cb){
@@ -65,13 +67,27 @@ app.route.post('/user/balance',async function(req,cb){
     address:req.query.address
    }
  var token=req.query.belriumtoken;
-   var response=await hlCall.call('GET',`/api/v1/balance`,params,token);
+   var response=await hlCall.call('GET','/api/v1/balance',params,token);
    return response;
 });
 app.route.post('/user/kycstatus',async function(req,cb){
     var belriumtoken = req.query.belriumtoken;
-    var response=await headerCall.call('GET',`/api/v1/user/countries/kyc`,belriumtoken);
+    var response=await headerCall.call('GET','/api/v1/user/countries/kyc',belriumtoken);
     return response;
 });
+app.route.post('/user/kycmapping',async function(req,cb){
+    var token = req.query.token;
+    var params={
+    kycDocumentMetaId:req.params.kycDocumentMetaId,
+    kycDocumentTypeId:req.params.kycDocumentTypeId
+    }
+    var response=await hlCall.call('GET','/api/v1/kycdocs/kycdocformfieldmetas?kycDocumentMetaId='+params.kycDocumentMetaId+'&kycDocumentTypeId='+params.kycDocumentTypeId+'&countryCode=IN',token);
+    return response;
+});
+
+
+
+
+
 
 
